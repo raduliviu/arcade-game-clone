8c97d99bd29349139c351f1a949b91cf2fe1d8c8
raduliviu <baltag.radu@gmail.com>
2018-01-26 13:26:14 +0100

    feat: Player sprite now on screen
    The player's avatar is now displayed on the board in its starting position

--- js/app.js
+++ js/app.js
@@ -24,9 +24,16 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function () {};
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+
+};
 Player.prototype.update = function () {};
-Player.prototype.render = function () {};
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+};
 Player.prototype.handleInput = function () {};
 
 // Now instantiate your objects.
